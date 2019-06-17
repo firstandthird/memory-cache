1f18148ea5ab6516e87a0209b7607bc1b2d61b94
orthagonal <orthagonal1@gmail.com>
2019-06-17 04:00:40 +0000

    instanceable, allowStale

--- test/test.api.js
+++ test/test.api.js
@@ -1,8 +1,9 @@
 const tap = require('tap');
-const { get, set, remove, removeAll, getStats, memo } = require('../');
+const spawn = require('../');
 const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 tap.test('set/get', t => {
+  const { get, set } = spawn();
   set('key', 1);
   const r = get('key');
   t.equal(r, 1);
@@ -10,6 +11,7 @@ tap.test('set/get', t => {
 });
 
 tap.test('set/get with ttl', async t => {
+  const { get, set } = spawn();
   set('key2', 1, 500);
   let r = get('key2');
   t.ok(r);
@@ -20,6 +22,7 @@ tap.test('set/get with ttl', async t => {
 });
 
 tap.test('remove', t => {
+  const { get, set, remove } = spawn();
   set('key3', 1);
   remove('key3');
   const r = get('key2');
@@ -28,6 +31,7 @@ tap.test('remove', t => {
 });
 
 tap.test('removeAll', t => {
+  const { get, set, removeAll } = spawn();
   set('key3', 1);
   set('key4', 1);
   removeAll();
@@ -39,33 +43,34 @@ tap.test('removeAll', t => {
 });
 
 tap.test('getStats', t => {
+  const { set, remove, getStats } = spawn();
   set('key3', 1);
   set('key4', 1);
   remove('key3');
   const s = getStats();
-  t.match(s, { hits: 2, misses: 4, sets: 7, removes: 2 });
+  t.match(s, { hits: 0, misses: 0, sets: 2, removes: 1 });
   t.end();
 });
 
-tap.test('memo', async t => {
-  let called = 0;
-  const fP = async() => {
-    called++;
-    await wait(100);
-    return 21;
-  };
-  let r1 = await memo('memo1', fP);
-  t.equal(called, 1);
-  t.equal(r1, 21);
-  r1 = await memo('memo1', fP);
-  t.equal(called, 1);
-  t.equal(r1, 21);
-  r1 = await memo('memo1', fP, 100, true);
-  t.equal(called, 2);
-  t.equal(r1, 21);
-  await wait(110);
-  r1 = await memo('memo1', fP);
-  t.equal(called, 3);
-  t.equal(r1, 21);
+tap.test('instances do not collide', t => {
+  const cache1 = spawn();
+  const cache2 = spawn();
+  cache1.set('key1', 1);
+  cache2.set('key1', 2);
+  const r1 = cache1.get('key1');
+  const r2 = cache2.get('key1');
+  t.equal(r1, 1);
+  t.equal(r2, 2);
+  t.end();
+});
+
+tap.test('set/get with allowStale', async t => {
+  const { get, set } = spawn();
+  set('key2', 1, 500);
+  let r = get('key2');
+  t.ok(r);
+  await wait(501);
+  r = get('key2');
+  t.ok(r, 'in allowStale mode the cache has to be manually refreshed');
   t.end();
 });
