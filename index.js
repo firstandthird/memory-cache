29178c3b50eb60bfc0297b74acbdf44153b00030
orthagonal <orthagonal1@gmail.com>
2019-06-17 21:08:23 +0000

    separate functions for allowStale

--- index.js
+++ index.js
@@ -2,25 +2,36 @@
 module.exports = (allowStale = false) => {
   let cache = {};
   const stats = { hits: 0, misses: 0, sets: 0, removes: 0 };
-  const get = function(key) {
-    const val = cache[key];
-    //if not exists:
-    if (!val) {
-      stats.misses++;
-      return null;
-    }
-    // if value has expired and its in the past
-    if (val.expires > 0 && val.expires < new Date().getTime()) {
-      // allow stale ignores ttl, so you must refresh the cache manually
-      if (allowStale) {
-        return val.value;
+  let get
+  if (allowStale) {
+    // allow stale ignores ttl, so you must refresh the cache manually
+    get = function(key) {
+      const val = cache[key];
+      //if not exists:
+      if (!val) {
+        stats.misses++;
+        return null;
       }
-      stats.misses++;
-      return null;
-    }
-    stats.hits++;
-    return val.value;
-  };
+      stats.hits++;
+      return val.value;
+    };
+  } else {
+    get = function(key) {
+      const val = cache[key];
+      //if not exists:
+      if (!val) {
+        stats.misses++;
+        return null;
+      }
+      // if value has expired and its in the past
+      if (val.expires > 0 && val.expires < new Date().getTime()) {
+        stats.misses++;
+        return null;
+      }
+      stats.hits++;
+      return val.value;
+    };
+  }
   const set = function(key, value, ttl) {
     stats.sets++;
     cache[key] = {
